89e800cd6aa75be6f09586ea3df067186a57474e
igrios1979 <ignarios1@hotmail.com>
2023-04-24 14:40:23 -0300

    Esta  es una prueba de test Unitarios con Junit

--- src/test/java/org/ignacio/rios/models/CuentaTest.java
+++ src/test/java/org/ignacio/rios/models/CuentaTest.java
@@ -1,7 +1,34 @@
 package org.ignacio.rios.models;
 
+//import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CuentaTest {
+
+    @Test
+    void test_nombre_cuenta(){
+        Cuenta cuenta = new Cuenta("ignacio",new BigDecimal("1000.2235"));
+      //  cuenta.setPersona("ignacio");
+        String esperado  = "ignacio";
+        String real = cuenta.getPersona();
+       assertEquals(esperado,real);
+       assertTrue(real.equals("ignacio"));
+
+    }
+
+@Test
+    void test_saldo(){
+        Cuenta cuenta = new Cuenta("ignacio",new BigDecimal("1000.2235"));
+        assertEquals(1000.2235,cuenta.getSaldo().doubleValue());
+        assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
+         assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
+
+}
+
 
 }
